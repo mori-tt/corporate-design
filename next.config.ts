d55427434932ea8c69c88834670d89ac06ef8b18
T_Mori <morit0301@gmail.com>
2025-03-27 20:46:41 +0900

    Configuring deployment to Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 // 静的生成用の設定
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? "/corporate-design" : "",
+  assetPrefix: isProd ? "/corporate-design/" : "",
   images: {
     unoptimized: true,
   },
